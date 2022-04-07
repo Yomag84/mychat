725c513ade35aa4269d18d24619f61ed64c4b4a7
Yomag84 <yomag84@gmail.com>
2022-04-07 09:24:58 +0100

    Master

---
+++ chat/views.py
@@ -0,0 +1,10 @@
+from multiprocessing import context
+from django.shortcuts import render
+
+# Create your views here.
+
+def main_view(request):
+    context={}
+
+    return render(request, 'chat/main.html', context=context)
+    
